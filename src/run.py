fcf3f1c35426757c1d367667ce6b613674dead27
Nico Krieger <26749617+GiantMolecularCloud@users.noreply.github.com>
2025-06-26 20:32:22 +0200

    implement plus devices

--- src/run.py
+++ src/run.py
@@ -8,12 +8,11 @@ import logging
 from pathlib import Path
 from time import sleep
 
-from __init__ import __author__, __version__
-
-from config import get_config
-from influx import Influx
-from shelly import Shelly
-from timer import RepeatedTimer
+from .config import get_config
+from .influx import Influx
+from .shelly import Shelly
+from .timer import RepeatedTimer
+from .version import __author__, __version__
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s -  %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger("main")
@@ -28,16 +27,6 @@ Link: https://github.com/GiantMolecularCloud/shelly-influx
 )
 parser.add_argument("configfile", metavar="config", type=Path, nargs="?", help="The configuration file to load.")
 parser.add_argument("--version", action="version", version=f"ShellyInflux {__version__}")
-args = parser.parse_args()
-
-
-config = get_config(args.configfile)
-
-if config.debug:
-    logger.setLevel("DEBUG")
-
-influx = Influx(config.influx, config.debug)
-shellies = [Shelly(device_config, config.debug) for device_config in config.devices]
 
 
 def query_and_push(shelly: Shelly, influx: Influx) -> None:
@@ -54,12 +43,27 @@ def query_and_push(shelly: Shelly, influx: Influx) -> None:
 
     try:
         shelly.query()
-        influx.write(shelly.influx_data)
+        if shelly.influx_data is None:
+            logger.warning(f"No data received from {shelly.name}.")
+        else:
+            influx.write(shelly.influx_data)
     except Exception as e:
         logger.error(e)
 
 
-def main():
+def main(argv: list[str] | None = None) -> None:
+
+    args = parser.parse_args(argv)
+    config = get_config(args.configfile)
+
+    logger.info("Loaded configuration:")
+    logger.info(config.model_dump_json(indent=4))
+
+    if config.debug:
+        logger.setLevel("DEBUG")
+
+    influx = Influx(config.influx, config.debug)
+    shellies = [Shelly(device_config, config.debug) for device_config in config.devices]
 
     timers = []
     for shelly in shellies:
@@ -73,7 +77,3 @@ def main():
     finally:
         for timer in timers:
             timer.stop()
-
-
-if __name__ == "__main__":
-    main()
